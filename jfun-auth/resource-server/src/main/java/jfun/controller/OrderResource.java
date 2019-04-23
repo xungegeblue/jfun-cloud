ea8304004d468688658702f9767c9c339cb80f0c
谢境勋 <787824374@qq.com>
2019-04-23 11:36:35 +0800

    解决请求资源服务器报错 1.主要是client_id配置错误 2.接口有问题不可以返回Object

--- jfun-auth/resource-server/src/main/java/jfun/controller/OrderResource.java
+++ jfun-auth/resource-server/src/main/java/jfun/controller/OrderResource.java
@@ -3,6 +3,9 @@ package jfun.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Author 谢镜勋
  * @Date 2019/4/23
@@ -10,7 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderResource {
     @GetMapping("order")
-    public Object order() {
-        return new Object();
+    public Map order() {
+        Map map = new HashMap();
+        map.put("data","jfun");
+        return map;
     }
 }
