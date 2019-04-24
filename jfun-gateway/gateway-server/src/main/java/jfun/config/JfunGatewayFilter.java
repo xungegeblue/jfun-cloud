52ed8f49f359acedc4de28b6d47f69570913343a
谢境勋 <787824374@qq.com>
2019-04-24 17:27:32 +0800

    准备集成网关

---
+++ jfun-gateway/gateway-server/src/main/java/jfun/config/JfunGatewayFilter.java
@@ -0,0 +1,18 @@
+package jfun.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author 谢镜勋
+ * @Date 2019/4/24
+ */
+@Component
+public class JfunGatewayFilter implements GlobalFilter {
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
---
+++ jfun-gateway/gateway-server/src/main/java/jfun/po/GatewayRouteDefinition.java
@@ -0,0 +1,14 @@
+package jfun.po;
+
+import org.springframework.cloud.gateway.route.RouteDefinition;
+
+import java.util.ArrayList;
+
+/**
+ * @Author 谢镜勋
+ * @Date 2019/4/24
+ */
+
+public class GatewayRouteDefinition {
+    RouteDefinition definition;
+}
