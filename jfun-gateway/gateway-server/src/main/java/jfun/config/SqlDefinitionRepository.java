d364e8ad8deeabc1b3ed967a964b3822f95b5e34
miv <787824374@qq.com>
2019-05-06 15:33:56 +0800

    数据库动态设置路由信息

--- jfun-gateway/gateway-server/src/main/java/jfun/config/SqlDefinitionRepository.java
+++ jfun-gateway/gateway-server/src/main/java/jfun/config/SqlDefinitionRepository.java
@@ -1,5 +1,13 @@
 package jfun.config;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import jfun.server.IRouteDefinitionService;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
 import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.cloud.gateway.route.RouteDefinitionRepository;
@@ -8,54 +16,60 @@ import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @Author 谢镜勋
  * @Date 2019/4/24
  */
 @Component
+@Slf4j
 public class SqlDefinitionRepository implements RouteDefinitionRepository {
+    @Autowired
+    IRouteDefinitionService iRouteDefinitionService;
+
+    @Override
     public Flux<RouteDefinition> getRouteDefinitions() {
 
-        List<RouteDefinition> routeList = new ArrayList<>();
+        log.info("获取路由信息...");
+        List<RouteDefinition> routeList = iRouteDefinitionService.getAll().stream().map(routeDefinitionEntity -> {
+            RouteDefinition d = new RouteDefinition();
 
-        RouteDefinition rd = getJfunRouteDefinitions();
-        routeList.add(rd);
+            d.setUri(URI.create(routeDefinitionEntity.getUri()));
+            d.setId(routeDefinitionEntity.getId()+"");
+            d.setOrder(routeDefinitionEntity.getOrders());
+
+            Gson gson = new Gson();
+            Type type = new TypeToken<ArrayList<FilterDefinition>>() {
+            }.getType();
+
+            List<FilterDefinition> filterDefinitions = gson.fromJson(routeDefinitionEntity.getFilters(), type);
+            d.setFilters(filterDefinitions);
+
+            type = new TypeToken<ArrayList<PredicateDefinition>>() {
+            }.getType();
+            List<PredicateDefinition> predicateDefinitions = gson.fromJson(routeDefinitionEntity.getPredicates(),type);
+            d.setPredicates(predicateDefinitions);
+            log.info("路由信息：{}",routeDefinitionEntity.toString());
+            return d;
+        }).collect(Collectors.toList());
+
+
         return Flux.fromIterable(routeList);
     }
-    //手动设置路由
-    private RouteDefinition getJfunRouteDefinitions() {
-        RouteDefinition rd = new RouteDefinition();
-        URI uri = UriComponentsBuilder.fromHttpUrl("http://httpbin.org:80/get").build().toUri();
-        rd.setId("test");
-        rd.setUri(uri);
 
-
-        List<PredicateDefinition> pds = new ArrayList<>();
-        PredicateDefinition pd = new PredicateDefinition();
-        Map<String,String> map = new HashMap<>();
-
-
-        map.put("pattern","/foo");
-        pd.setName("Path");
-        pd.setArgs(map);
-
-        pds.add(pd);
-        rd.setPredicates(pds);
-//        rd.setOrder();
-//        rd.setFilters();
-        return rd;
-    }
-
+    @Override
     public Mono<Void> save(Mono<RouteDefinition> route) {
         return null;
     }
 
+    @Override
     public Mono<Void> delete(Mono<String> routeId) {
         return null;
     }
