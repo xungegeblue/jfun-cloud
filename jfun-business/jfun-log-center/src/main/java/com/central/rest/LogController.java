38c7243f8713f63a533aa4f99bfc132bba3745cd
miv <787824374@qq.com>
2019-06-24 02:45:35 +0800

    去除target文件

--- jfun-business/jfun-log-center/src/main/java/com/central/rest/LogController.java
+++ jfun-business/jfun-log-center/src/main/java/com/central/rest/LogController.java
@@ -1,5 +1,6 @@
 package com.central.rest;
 
+import org.apache.skywalking.apm.toolkit.trace.Trace;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LogController {
 
-
-
+    @Trace
     @GetMapping("log")
     public Object log() {
         return "log";
